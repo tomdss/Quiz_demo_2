cd4f452ce8914253305a37ba96162f7eeedf3d9b
Tong Tat Liem <liemtt@rikkeisoft.com>
2019-04-24 18:25:16 +0700

    commit second

--- app/src/main/java/com/example/quiztestdemo2/MainActivity.java
+++ app/src/main/java/com/example/quiztestdemo2/MainActivity.java
@@ -4,11 +4,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button btnOk;
+    private EditText edtFirst;
+    private RadioGroup rgTwo;
+    private RadioGroup rgThree;
+    private RadioGroup rgFour;
+    private RadioGroup rgFive;
+    private RadioGroup rgSix;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,12 +28,39 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private void initViews() {
 
         btnOk = findViewById(R.id.btn_ok);
+        edtFirst = findViewById(R.id.edt_first);
+        rgTwo = findViewById(R.id.rg_two);
+        rgThree = findViewById(R.id.rg_three);
+        rgFour = findViewById(R.id.rg_four);
+        rgFive = findViewById(R.id.rg_five);
+        rgSix = findViewById(R.id.rg_six);
+
         btnOk.setOnClickListener(this);
 
     }
 
     @Override
     public void onClick(View v) {
-        Toast.makeText(this, "Success", Toast.LENGTH_SHORT).show();
+        int  m ;
+        m = checkQuest();
+        if(m==6)
+        Toast.makeText(this, "Success " +m+"/6", Toast.LENGTH_SHORT).show();
+        else {
+            Toast.makeText(this, "Fail " +m+"/6", Toast.LENGTH_SHORT).show();
+        }
+//        rgOne.getCheckedRadioButtonId();
+    }
+
+    private int checkQuest() {
+        int i = 0;
+
+        if(!edtFirst.getText().toString().isEmpty())i++;
+        if(rgTwo.getCheckedRadioButtonId()!=-1)i++;
+        if(rgThree.getCheckedRadioButtonId()!=-1)i++;
+        if(rgFour.getCheckedRadioButtonId()!=-1)i++;
+        if(rgFive.getCheckedRadioButtonId()!=-1)i++;
+        if(rgSix.getCheckedRadioButtonId()!=-1)i++;
+
+        return i;
     }
 }
